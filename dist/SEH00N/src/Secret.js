262944374eb61b4401d6a7da927280b4a1153881
kwak1s1h <kwak1s1h@kakao.com>
2023-09-07 01:44:52 +0900

    파일정리

---
+++ dist/SEH00N/src/Secret.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.dbConfig = void 0;
+exports.dbConfig = {
+    host: 'gondr99.iptime.org',
+    user: 'sehoon',
+    password: '2006psh0501',
+    database: 'sehoon',
+    port: 3306
+};
---
+++ dist/SEH00N/src/types.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.MealType = void 0;
+var MealType;
+(function (MealType) {
+    MealType[MealType["Breakfast"] = 0] = "Breakfast";
+    MealType[MealType["Lunch"] = 1] = "Lunch";
+    MealType[MealType["Dinner"] = 2] = "Dinner";
+})(MealType = exports.MealType || (exports.MealType = {}));
